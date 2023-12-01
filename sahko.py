3d277509719aa77f97b21c9e836283267c9859b8
arzca <tanner.ari@outlook.com>
2023-12-01 22:15:03 +0200

    db created and csv data imported to db

--- sahko.py
+++ sahko.py
@@ -3,8 +3,7 @@ Sähkön hinta
 '''
 
 import sys, csv
-import matplotlib.pyplot as plt
-import numpy as np
+import pymongo
 
 # Read 'data.csv' file and return a list
 def read_csv_file():
@@ -16,40 +15,6 @@ def read_csv_file():
         for row in csv_reader:
             list_return.append(row)
         return list_return
-
-# Draw graph
-def draw_graph(usage_list):
-    x = []
-    y = []
-    row = 0
-    for i in usage_list:
-        if row == 0:
-            row += 1
-        else:
-            x.append(i[0])
-            y.append(float(i[1]))
-
-    plt.plot(x, y, color='green', linestyle='dashed', marker='o', \
-    markerfacecolor='blue', markersize=6)
-    plt.xlabel('Kuukausi')
-    plt.ylabel('kWh')
-    plt.title('Sähkönkulutus')
-    plt.show()
-
-def draw_price(usage_list):
-    x = []
-    y = []
-    row = 0
-    for i in usage_list:
-        if row == 0:
-            row += 1
-        else:
-            x.append(i[0])
-            y.append(float(i[2]))
-    month = np.array(x)
-    price = np.array(y)
-    plt.bar(month,price)
-    plt.show()
 
 def print_csv_file(usage_list):
     row = 0
@@ -77,21 +42,31 @@ def write_csv_to_file(usage_list):
 
     print('data saved!')
 
+def add_to_db(usage_list, db_collection):
+    # delete existing db first to insert new one from csv
+    db_collection.delete_many({})
+
+    # create new db from csv
+    db_list = []
+    id = 1
+    for i in range(1, len(usage_list)):
+        db_list.append({'_id': id, 'month': usage_list[i][0], 'consumption': usage_list[i][1], 'price': usage_list[i][2]})
+        id += 1
+
+    db_collection.insert_many(db_list)
+
 def main():
 
-    # Test if data.csv exists
-    try:
-        usage_list = read_csv_file()
-    except Exception as e:
-        print("Tiedosto 'data.csv' ei ole olemassa")
-        sys.exit(0)
+    # create and use db
+    db_client = pymongo.MongoClient('mongodb://localhost:27017')
+    mydb = db_client['power_db'] # define db
+    db_collection = mydb['month'] # collection
 
     # Run until 'Lopeta'
     while True:
         print('''
-(1) Lue CSV data
+(1) Tuo CSV data (data.csv)
 (2) Lisää data
-(3) Piirrä data
 (4) Lopeta
 
 Mitä haluat tehdä? ''', end='')
@@ -99,12 +74,12 @@ Mitä haluat tehdä? ''', end='')
         valinta = int(input())
 
         if valinta == 1:
-            print_csv_file(usage_list)
+            usage_list = read_csv_file()
+            add_to_db(usage_list, db_collection)
         elif valinta == 2:
             add_data(usage_list)
         elif valinta == 3:
-            draw_graph(usage_list)
-            draw_price(usage_list)
+            print_csv_file(usage_list)
         elif valinta == 4:
             write_csv_to_file(usage_list)
             sys.exit(0)
