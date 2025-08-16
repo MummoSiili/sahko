c7d4e9a6f2bdbc963cd94476101aef31b6dc8d5f
arzca <tanner.ari@outlook.com>
2025-08-16 19:17:25 +0300

    two methods added.

--- sahkov2.py
+++ sahkov2.py
@@ -3,9 +3,16 @@ class sahkoData:
         self.kwh = kwh
         self.kwh_price = kwh_price / 100 # Price in euros
 
-
     def __str__(self):
         return f'Consumed {self.kwh} with total price {self.kwh_price}'
 
+    def monthly_cost(self):
+        self.cost = self.kwh * self.kwh_price
+
+    def show_monthly_cost(self):
+        return f'Monthly cost {self.cost:.2f}e'
+
 eka = sahkoData(600.78, 3.75)
 print(eka)
+eka.monthly_cost()
+print(eka.show_monthly_cost())
